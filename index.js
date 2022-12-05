6edb0aa8409b32c82bb2fe298eb8e7d34514556a
RoyJumah <royjumah17@gmail.com>
2022-12-05 16:08:20 +0300

    Add index.js to awesome-books

---
+++ index.js
@@ -0,0 +1,28 @@
+import Book from "./Book.js";
+import UI from "./modules/UI.js";
+import Bookstore from "./modules/Bookstore.js";
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+
+  const book = new Book(title, author);
+  UI.addBook(book);
+  Bookstore.addBook(book);
+
+  form.reset();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  UI.displayBooks();
+});
+
+document.getElementById("bookshelf").addEventListener("click", (event) => {
+  UI.removeBook(event.target);
+  Bookstore.removeBook(
+    event.target.previousElementSibling.previousElementSibling.textContent
+  );
+});
